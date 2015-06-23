3997e51dd56535e76b4a8565425fb70fb1a37fe6
Paul Oliver <puzza007@gmail.com>
2015-06-23 11:21:41 +0200

    Log body when jsx:decode fails

--- src/erlasticsearch_worker.erl
+++ src/erlasticsearch_worker.erl
@@ -319,6 +319,7 @@ process_response(false, #restResponse{status = Status, body = Body}) ->
         [{status, Status}, {body, jsx:decode(Body)}]
     catch
         error:badarg ->
+            lager:error("Failed to decode ~p", [Body]),
             [{status, Status}, {body, Body}]
     end.
 
